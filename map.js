cbd5999dc7e05d4df0d56699d07bdc5016de8084
Vilian276 <baduokova@uni-sofia.bg>
2023-05-17 20:07:56 +0300

    created web map; added home button;

---
+++ map.js
@@ -0,0 +1,29 @@
+require([
+    "esri/config",
+    "esri/WebMap",
+    "esri/views/MapView",
+    "esri/widgets/ScaleBar",
+    "esri/widgets/Legend",
+    "esri/widgets/Home"
+], (esriConfig, WebMap, MapView, ScaleBar, Legend, Home) => {
+
+    esriConfig.apiKey = "AAPK620b0cee70ce48c99fc9577ff49c118dgUkDhdBeqioif9u0sPXLgfo4ypzs-znKFKvSDBzK2Ui3WQFCUJji8t3amJoQPSyR";
+
+    const webMap = new WebMap({
+        portalItem: {
+            id: "232b4d297d054b2a831a3ce629ac8495"
+        }
+    });
+
+    const view = new MapView({
+        container: "viewDiv",
+        map: webMap
+    });
+
+    const homeBtn = new Home({
+        view: view
+    });
+
+    view.ui.add(homeBtn, "top-left");
+
+});
