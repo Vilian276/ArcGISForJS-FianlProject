eb50a94b0c604bd955a5b01300ba1f97f358ee87
Vilian276 <baduokova@uni-sofia.bg>
2023-05-17 23:51:05 +0300

    added layer list button on/off toggle;

--- map.js
+++ map.js
@@ -4,8 +4,9 @@ require([
     "esri/views/MapView",
     "esri/widgets/ScaleBar",
     "esri/widgets/Legend",
-    "esri/widgets/Home"
-], (esriConfig, WebMap, MapView, ScaleBar, Legend, Home) => {
+    "esri/widgets/Home",
+    "esri/widgets/LayerList"
+], (esriConfig, WebMap, MapView, ScaleBar, Legend, Home, LayerList) => {
 
     esriConfig.apiKey = "AAPK620b0cee70ce48c99fc9577ff49c118dgUkDhdBeqioif9u0sPXLgfo4ypzs-znKFKvSDBzK2Ui3WQFCUJji8t3amJoQPSyR";
 
@@ -40,16 +41,25 @@ require([
 
     view.ui.add(scaleBar, "bottom-right");
 
-    view.ui.add("layerList", "top-right");
+    view.ui.add("layerList-btn", "top-right");
+
+    const layerList = new LayerList({
+        view: view
+    })
+
+    view.ui.add(layerList, "top-right");
 
     document
-        .getElementById("layerList")
+        .getElementById("layerList-btn")
         .addEventListener("click", function() {
-
+            toggleButton()
         })
 
     function toggleButton() {
+        const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
+        let currentProp = layerListEl.style.getPropertyValue("display");
 
+        layerListEl.style.setProperty("display", currentProp == "block" ? "none" : "block0");
     };
 
 });
